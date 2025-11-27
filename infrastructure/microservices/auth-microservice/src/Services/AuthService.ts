cde5f6a0db30947488481a2c113d5aef33102473
Laketaa <luka.laketiic@gmail.com>
2025-11-27 20:47:42 +0100

    Implementiran mikroservis za autentifikaciju iz opste specifikacije, sa svim validacijama, sa login-om, register-om i koristi DB korisnici

--- infrastructure/microservices/auth-microservice/src/Services/AuthService.ts
+++ infrastructure/microservices/auth-microservice/src/Services/AuthService.ts
@@ -21,6 +21,11 @@ export class AuthService implements IAuthService {
     const passwordMatches = await bcrypt.compare(data.password, user.password);
     if (!passwordMatches) return { authenificated: false };
 
+    // Oba polja se moraju popuniti
+    if (!data.username || !data.password) {
+      return { authenificated: false };
+    }
+
     return {
       authenificated: true,
       userData: {
@@ -35,15 +40,34 @@ export class AuthService implements IAuthService {
    * Register new user
    */
   async register(data: RegistrationUserDTO): Promise<AuthResponseType> {
-    // Check if username or email already exists
+
+    // Provera da li korsisnik sa datim imenom ili emailom vec postoji
     const existingUser = await this.userRepository.findOne({
       where: [{ username: data.username }, { email: data.email }],
     });
 
     if (existingUser) return { authenificated: false };
 
+    // Xesiranje lozinke
     const hashedPassword = await bcrypt.hash(data.password, this.saltRounds);
 
+    // Osnovna validacija za obavezna polja
+    if (!data.username || !data.password || !data.email || !data.firstName || !data.lastName || !data.role) {
+      return { authenificated: false };
+    }
+
+    // Regeks provera za e-mail
+    const emailRegex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/i;
+    if (!emailRegex.test(data.email)) {
+      return { authenificated: false };
+    }
+
+    // Minimalna duzina lozinke
+    if (data.password.length < 6) {
+      return { authenificated: false };
+    }
+
+    // Dodavanje novog korisnika
     const newUser = this.userRepository.create({
       username: data.username,
       firstName: data.firstName,
